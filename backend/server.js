b58d995bc5441de8b140b9f445152d40a6659950
Shivam Kumar <sk744826744@gmail.com>
2025-03-09 10:37:39 +0530

    file update

--- backend/server.js
+++ backend/server.js
@@ -29,3 +29,6 @@ app.get("/verify/:serial_no", (req, res) => {
         res.json(result[0]);
     });
 });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
